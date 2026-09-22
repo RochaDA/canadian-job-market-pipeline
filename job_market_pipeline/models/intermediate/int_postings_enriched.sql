5f5c52c211d9b0caa7b3168539394222020abc6c
HugoR <47538515+RochaDA@users.noreply.github.com>
2026-09-21 17:27:26 -0700

    Standardize NOC code/name nulls to 'Not specified', document unresolved 2-row NOC16/21 mismatch

--- job_market_pipeline/models/intermediate/int_postings_enriched.sql
+++ job_market_pipeline/models/intermediate/int_postings_enriched.sql
@@ -9,10 +9,10 @@ enriched as (
     select
         job_posting_id,
         job_title,
-        noc16_code,
-        noc16_code_name,
-        noc21_code,
-        noc21_code_name,
+        case when noc16_code is null then 'Not specified' else noc16_code end as noc16_code,
+        case when noc16_code_name is null then 'Not specified' else noc16_code_name end as noc16_code_name,
+        case when noc21_code is null then 'Not specified' else noc21_code end as noc21_code,
+        case when noc21_code_name is null then 'Not specified' else noc21_code_name end as noc21_code_name,
         posting_date,
         vacancy_count,
         case
@@ -32,27 +32,26 @@ enriched as (
         employment_type,
         employment_term,
         salary_condition_detail,
-        salary_period                                              as salary_period_raw,
+        salary_period as salary_period_raw,
         case
             when salary_period is not null then salary_period
             when salary_condition_detail like '%commission%' then null
             when salary_min is not null and salary_min < 1000 then 'Hour'
             when salary_min is not null and salary_min >= 1000 then 'Year'
             else null
-        end                                                          as salary_period,
+        end as salary_period,
         case
             when salary_period is null
                 and salary_condition_detail not like '%commission%'
                 and salary_min is not null
             then true
             else false
-        end                                                          as salary_period_was_inferred,
+        end as salary_period_was_inferred,
         salary_min,
         salary_max,
         hours_per,
         hours_min,
         hours_max,
-
         case
             when education_los is null then 'minimal'
             else 'detailed'
